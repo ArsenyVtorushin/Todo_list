0719d1f7acac29b2aa46a70a96bb28457cf85f4e
Top <Top@WIN-OAC3RGNJ0PA>
2025-10-07 20:43:51 +0500

    Добавьте файлы проекта.

---
+++ Todo_list/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Todo_list
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dateToDo.SelectedDate = new DateTime(2024, 01, 10);
+            descriptionToDo.Text = "Описания нет";
+
+            groupBoxToDo.Visibility = Visibility.Hidden;
+        }
+
+        private void AddNewTask_Checked(object sender, RoutedEventArgs e)
+        {
+            groupBoxToDo.Visibility = Visibility.Visible;
+        }
+
+        private void AddNewTask_Unchecked(object sender, RoutedEventArgs e)
+        {
+            groupBoxToDo.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ Todo_list/ToDo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo_list
+{
+    public class ToDo
+    {
+        private string _title;
+        private DateTime _date;
+        private string _description;
+
+        public ToDo()
+        {
+
+        }
+
+        public string Title { get { return _title; } set { _title = value; } }
+        public DateTime Date { get { return _date; } set { _date = value; } }
+        public string Description { get { return _description;} set { _description = value; } }
+    }
+}
